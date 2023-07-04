178f97061bb200cd538261089fee73f44d320b7e
Vladimir Blinnikov <blinnik2001@yandex.ru>
2023-07-04 16:08:09 +0300

    fix: fix class annotations

--- src/main/java/ru/practicum/shareit/request/model/dto/RequestDescription.java
+++ src/main/java/ru/practicum/shareit/request/model/dto/RequestDescription.java
@@ -1,16 +1,11 @@
 package ru.practicum.shareit.request.model.dto;
 
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.experimental.FieldDefaults;
 import org.hibernate.validator.constraints.Length;
 
 import javax.validation.constraints.NotNull;
 
 @Getter
-@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
-@AllArgsConstructor
 public class RequestDescription {
     @NotNull
     @Length(message = "Описание запроса на предмет должно содержать от 10 до 500 символов", min = 10, max = 500)
