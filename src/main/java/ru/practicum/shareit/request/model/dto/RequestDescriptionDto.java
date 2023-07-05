4f89231a7c40fd92ae7ac6c823edb2916df9b802
Vladimir Blinnikov <blinnik2001@yandex.ru>
2023-07-06 02:28:06 +0300

    feat: add new annotations to RequestDescriptionDto for tests

--- src/main/java/ru/practicum/shareit/request/model/dto/RequestDescriptionDto.java
+++ src/main/java/ru/practicum/shareit/request/model/dto/RequestDescriptionDto.java
@@ -1,11 +1,16 @@
 package ru.practicum.shareit.request.model.dto;
 
-import lombok.Getter;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.hibernate.validator.constraints.Length;
 
 import javax.validation.constraints.NotNull;
 
-@Getter
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class RequestDescriptionDto {
     @NotNull
     @Length(message = "Описание запроса на предмет должно содержать от 10 до 500 символов", min = 10, max = 500)
