bcc4ffe63f0ae98009a5e3a740f9ed96651208e1
Vladimir Blinnikov <blinnik2001@yandex.ru>
2023-07-06 02:12:23 +0300

    feat: add annotations to DTO classes (for tests)

--- src/main/java/ru/practicum/shareit/item/model/dto/CommentTextDto.java
+++ src/main/java/ru/practicum/shareit/item/model/dto/CommentTextDto.java
@@ -1,9 +1,11 @@
 package ru.practicum.shareit.item.model.dto;
 
-import lombok.Getter;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.hibernate.validator.constraints.Length;
 
-@Getter
+@Data
+@NoArgsConstructor
 public class CommentTextDto {
     @Length(message = "Комментарий должен содержать от 5 до 500 символов", min = 5, max = 500)
     String text;
