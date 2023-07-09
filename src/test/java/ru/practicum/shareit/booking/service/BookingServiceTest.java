72ccc24ac518d6455659979dfd53a1429c385c54
Vladimir Blinnikov <blinnik2001@yandex.ru>
2023-07-09 05:34:56 +0300

    perf: delete unused import

--- src/test/java/ru/practicum/shareit/booking/service/BookingServiceTest.java
+++ src/test/java/ru/practicum/shareit/booking/service/BookingServiceTest.java
@@ -27,7 +27,6 @@ import ru.practicum.shareit.common.exception.NotFoundException;
 import ru.practicum.shareit.common.exception.NotOwnerException;
 import ru.practicum.shareit.common.model.PaginationConfig;
 import ru.practicum.shareit.item.model.Item;
-import ru.practicum.shareit.item.model.dto.ItemBookingsAndCommentsDto;
 import ru.practicum.shareit.item.repository.ItemRepository;
 import ru.practicum.shareit.user.model.User;
 import ru.practicum.shareit.user.repository.UserRepository;
