ee7c5f63c25eb331af0de3001e86317ed59c97a8
Vladimir Blinnikov <blinnik2001@yandex.ru>
2023-07-09 15:18:26 +0300

    test: add an integration test that verifies the operation of the getAllByTextQuery method

--- src/test/java/ru/practicum/shareit/item/service/ItemServiceIntegrationTest.java
+++ src/test/java/ru/practicum/shareit/item/service/ItemServiceIntegrationTest.java
@@ -13,6 +13,7 @@ import ru.practicum.shareit.booking.model.dto.BookingItemIdAndTimeDto;
 import ru.practicum.shareit.booking.service.BookingService;
 import ru.practicum.shareit.common.model.PaginationConfig;
 import ru.practicum.shareit.item.model.dto.ItemBookingsDto;
+import ru.practicum.shareit.item.model.dto.ItemDto;
 import ru.practicum.shareit.item.model.dto.ItemRequestIdDto;
 import ru.practicum.shareit.user.model.User;
 import ru.practicum.shareit.user.model.dto.UserDto;
@@ -38,9 +39,8 @@ class ItemServiceIntegrationTest {
     PaginationConfig paginationConfig = new PaginationConfig();
 
     @Test
-    void getAllByOwnerId_whenUserFound_thenReturnUserItem() {
+    void getAllByOwnerId_whenUserFound_thenReturnUserItems() {
         // GIVEN
-
         // Создаем пользователей
         UserDto userDto = UserDto.builder().name("test").email("test@mail.com").build();
         UserDto userDto2 = UserDto.builder().name("test2").email("test2@mail.com").build();
@@ -142,5 +142,75 @@ class ItemServiceIntegrationTest {
 
         assertEquals(expectedUser1ItemBookingsDtos, actualUser1ItemBookingsDtos);
         assertEquals(expectedUser2ItemBookingsDtos, actualUser2ItemBookingsDtos);
+    }
+
+    @Test
+    void getAllByTextQuery_whenUserFound_thenReturnSearchableItems() {
+        // GIVEN
+        // Создаем пользователей
+        UserDto userDto = UserDto.builder().name("test").email("test@mail.com").build();
+        UserDto userDto2 = UserDto.builder().name("test2").email("test2@mail.com").build();
+        User user = userService.create(userDto);
+        User user2 = userService.create(userDto2);
+        Long user1Id = user.getId();
+        Long user2Id = user2.getId();
+
+        // Создаем предметы для пользователя 1
+        ItemRequestIdDto itemRequestIdDto = ItemRequestIdDto.builder()
+                .name("Дрель")
+                .description("test item description")
+                .available(true)
+                .build();
+
+        ItemRequestIdDto itemRequestIdDto2 = ItemRequestIdDto.builder()
+                .name("test item name2")
+                .description("Самокат")
+                .available(false)
+                .build();
+
+        itemRequestIdDto = itemService.create(user1Id, itemRequestIdDto);
+        itemService.create(user1Id, itemRequestIdDto2);
+
+        // Создаем предметы для пользователя 2
+        ItemRequestIdDto itemRequestIdDto3 = ItemRequestIdDto.builder()
+                .name("Пылесос")
+                .description("test item description3")
+                .available(true)
+                .build();
+
+        ItemRequestIdDto itemRequestIdDto4 = ItemRequestIdDto.builder()
+                .name("test item name4")
+                .description("Шуруповёрт")
+                .available(true)
+                .build();
+
+        itemService.create(user2Id, itemRequestIdDto3);
+        itemRequestIdDto4 = itemService.create(user2Id, itemRequestIdDto4);
+
+        // WHEN
+        List<ItemDto> actualUser1ItemDtos = itemService.getAllByTextQuery(user1Id, "шуруП", paginationConfig);
+        List<ItemDto> actualUser2ItemDtos = itemService.getAllByTextQuery(user2Id, "дре", paginationConfig);
+
+        // THEN
+        List<ItemDto> expectedUser1ItemDtos = List.of(
+                ItemDto.builder()
+                        .id(itemRequestIdDto4.getId())
+                        .name(itemRequestIdDto4.getName())
+                        .description(itemRequestIdDto4.getDescription())
+                        .available(itemRequestIdDto4.getAvailable())
+                        .build()
+        );
+
+        List<ItemDto> expectedUser2ItemDtos = List.of(
+                ItemDto.builder()
+                        .id(itemRequestIdDto.getId())
+                        .name(itemRequestIdDto.getName())
+                        .description(itemRequestIdDto.getDescription())
+                        .available(itemRequestIdDto.getAvailable())
+                        .build()
+        );
+
+        assertEquals(expectedUser1ItemDtos, actualUser1ItemDtos);
+        assertEquals(expectedUser2ItemDtos, actualUser2ItemDtos);
     }
 }
